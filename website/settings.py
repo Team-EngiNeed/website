548f5fdf38f88e19d1890d4aaf0b83ed8154a055
MashleMashleMashleMashle <nicoangeloilustre@gmail.com>
2024-11-15 01:03:14 +0800

    Final push

--- website/settings.py
+++ website/settings.py
@@ -10,16 +10,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = "hotdog"
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = "hotdog"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+#DEBUG = True
 
 
-#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
+#ALLOWED_HOSTS = []
 
 
 
