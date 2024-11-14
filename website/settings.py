07e190da0750239c5ba8c0898d5ff541f662de3e
MashleMashleMashleMashle <nicoangeloilustre@gmail.com>
2024-11-15 01:07:18 +0800

    Final push like legit

--- website/settings.py
+++ website/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     }
 }
 
-#database_url = os.environ.get("DATABASE_URL")
-#DATABASES["default"]=dj_database_url.parse(database_url)
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"]=dj_database_url.parse(database_url)
 
 
 #database_url = os.environ.get("DATABASE_URL")
