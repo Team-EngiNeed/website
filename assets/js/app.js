b6b603349c9e1fe2bf25682c20f3af39bcc60404
MashleMashleMashleMashle <nicoangeloilustre@gmail.com>
2024-12-01 21:58:06 +0800

    Second round

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,23 @@
 // Selecting DOM elements for menu toggle
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
 // Toggle the menu
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+
+const passwordField = document.querySelector("input[type='password']");
+const showPasswordCheckbox = document.getElementById("showPassword");
+
+// Add an event listener to the checkbox to toggle the password visibility
+showPasswordCheckbox.addEventListener("change", function () {
+  if (this.checked) {
+    // Show the password
+    passwordField.type = "text";
+  } else {
+    // Hide the password
+    passwordField.type = "password";
+  }
 });
