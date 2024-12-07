9e8ce14eb75526aa971978b44d9c8d061c03ea45
Team-EngiNeed <teamengineed@gmail.com>
2024-12-07 16:58:14 +0800

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -10,15 +10,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = 'haha'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = 'haha'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+#DEBUG = True
 
-#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
+#ALLOWED_HOSTS = []
 
 
 
@@ -77,8 +77,8 @@ DATABASES = {
     }
 }
 
-#database_url = os.environ.get("DATABASE_URL")
-#DATABASES["default"]=dj_database_url.parse(database_url)
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"]=dj_database_url.parse(database_url)
 
 
 # Password validation
